da00bf17f602b0612ff400439b0c2be555733d7d
andrepolischuk <andre.polischuk@gmail.com>
2015-07-04 16:09:44 +0300

    Refactor

--- index.js
+++ index.js
@@ -14,64 +14,43 @@ var mime = require('mime');
  * Expose server
  */
 
-module.exports = Servst;
-
-/**
- * Serve object
- * @param  {String} root
- * @return {Function}
- * @api private
- */
-
-function Servst(root) {
-  if (!(this instanceof Servst)) return new Servst(root);
+module.exports = function(root) {
   if (typeof root !== 'string') return;
+  root = path.normalize(root);
 
-  this.root = path.normalize(root);
-  return this.serve.bind(this);
-}
+  return function(req, res, fn) {
+    if (!/^(GET|HEAD)$/.test(req.method)) return fn();
+    var filePath = decodeURIComponent(url.parse(req.url).pathname);
+    if (!path.extname(filePath)) return fn();
 
-/**
- * Serve file
- * @param  {Object}   req
- * @param  {Object}   res
- * @param  {Function} fn
- * @return {Mixed}
- * @api private
- */
+    if (~filePath.indexOf('\0')) {
+      fn({
+        url: filePath,
+        status: 400
+      });
+    }
 
-Servst.prototype.serve = function(req, res, fn) {
-  if (!/^(GET|HEAD)$/.test(req.method)) return fn();
-  var filePath = decodeURIComponent(url.parse(req.url).pathname);
-  if (!path.extname(filePath)) return fn();
+    filePath = path.normalize(path.join(root, filePath));
 
-  if (~filePath.indexOf('\0')) {
-    fn({
-      url: filePath,
-      status: 400
-    });
-  }
-
-  filePath = path.normalize(path.join(this.root, filePath));
-
-  if (filePath.indexOf(this.root) !== 0) {
-    fn({
-      url: filePath,
-      status: 404
-    });
-  }
-
-  fs.exists(filePath, function(exist) {
-    if (!exist) {
+    if (filePath.indexOf(root) !== 0) {
       fn({
         url: filePath,
         status: 404
       });
-      return;
     }
 
-    var mimeType = mime.lookup(filePath);
-    res.writeHead(200, {'Content-Type': mimeType + '; charset=utf-8'});
-    fs.createReadStream(filePath).pipe(res);
-  });
+    fs.exists(filePath, function(exist) {
+      if (!exist) {
+        fn({
+          url: filePath,
+          status: 404
+        });
+        return;
+      }
+
+      var mimeType = mime.lookup(filePath);
+      res.writeHead(200, {'Content-Type': mimeType + '; charset=utf-8'});
+      fs.createReadStream(filePath).pipe(res);
+    });
+  }
 };
